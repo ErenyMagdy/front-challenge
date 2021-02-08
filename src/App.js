acda217f798ca3a7dd54fd5373daa6b1b1b385d3
ereny <erenymagdy@gmail.com>
2021-02-08 15:53:01 -0800

    fetching repositories

--- src/App.js
+++ src/App.js
@@ -1,17 +1,71 @@
 import './App.css';
-
-function App() {
+import React, { Component } from 'react';
+import {  Media } from 'reactstrap';
+//import axios from 'axios';
+function RenderReposritory(props) {
   return (
-    <div className="App" >
-      <h1>Welcome To Front-End Coding Challenge</h1>
-      <div className="row"> 
-        <div className="col-6 text-primary">check bootstrap
-        </div>
-        <div className="col-6">check fontawesome <i class="fa fa-smile-o" aria-hidden="true"></i>
-        </div>
+    <React.Fragment>
+      <div className="container"> 
+        { props.repositories.map(rep =>
+          <div key={rep.id} className=' mt-5'>
+            <Media list>
+              <Media left key={rep.id} className="col-md-4">
+                <Media object width="30%" 
+                  src={rep.owner.avatar_url} 
+                  alt={rep.owner.login} 
+                  title={rep.owner.login} />
+              </Media>
+              <Media  tag='li'>
+                <Media  body className='ml-5'>
+                  <Media heading> Repository name :{rep.name}</Media>
+                  <p className='mb-2'> Number of stars : {rep.stargazers_count}</p>
+                  <p> Repository description : {rep.description}</p>
+                  <p>Number of issues : {rep.open_issues_count}</p>
+                  <p>Submited 30 days ago by {rep.owner.login}</p>
+                </Media>
+              </Media>
+            </Media> 
+          </div>
+        )}           
       </div>
-    </div>
+    </React.Fragment>
   );
 }
-
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = 
+    {
+      repositories: [],
+      loading: true
+    };
+  }
+  async componentDidMount() {
+    fetch('https://api.github.com/search/repositories?q=created:%3E2017-10-22&sort=stars&order=desc')
+    .then(result => result.json())
+    .then(repositories => this.setState({repositories: repositories.items, loading: false }))
+    //const {repositories}  = await axios.get('https://api.github.com/search/repositories?q=created:%3E2017-10-22&sort=stars&order=desc');
+    //const repositories = await response.json();
+    //this.setState({ repositories: repositories.items });
+    //console.log(data.items[0].description);
+  }
+  render() {
+    if (this.state.loading) {
+      return (
+        <div className="container m-5">
+          <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></span>
+          <div>loading...</div>
+        </div>
+      );
+    }
+    else if (!this.state.repositories) {
+      return <div>didn't get a repository</div>;
+    }
+    else if(this.state.repositories != null) {
+      return (
+        <RenderReposritory repositories = {this.state.repositories}/>
+      );
+    }
+  }
+}
 export default App;
